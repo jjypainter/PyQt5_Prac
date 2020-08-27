3433c872edfdc8abeae37279325c864860bbb5c3
jjypainter <jypainter@naver.com>
2020-08-28 06:15:16 +0900

    PyQt5공부자료 생성및 깃 이그노어 생성. 공부는 현재 메뉴창까지 함

---
+++ PyQt02.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 28 05:53:50 2020
+
+@author: ganglim
+"""
+
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QToolTip
+from PyQt5.QtGui import QFont
+
+class MyApp(QWidget):
+    
+    
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+        
+    def initUI(self):
+        QToolTip.setFont(QFont('SansSerif', 10))
+        self.setToolTip('This is a <b>QWidget</b> widget')
+        
+        
+        btn = QPushButton('Button', self)
+        btn.setToolTip('This is a <b>QPushButton</b> widget')
+        btn.move(50, 50)
+        btn.resize(btn.sizeHint())
+        
+        
+        self.setWindowTitle('ToolTips')
+        self.setGeometry(300, 300, 300, 200)
+        self.show()
+        
+        
+        
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyApp()
+    sys.exit(app.exec_())
